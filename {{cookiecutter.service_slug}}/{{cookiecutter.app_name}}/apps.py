5443361d39b3678c561004fc70bf358900eddc2c
ndongchrist <christianhonore2003@gmail.com>
2026-06-27 11:13:48 +0100

    fix: distinct app label to avoid django.contrib.auth collision

--- {{cookiecutter.service_slug}}/{{cookiecutter.app_name}}/apps.py
+++ {{cookiecutter.service_slug}}/{{cookiecutter.app_name}}/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class {{ cookiecutter.app_name|capitalize }}Config(AppConfig):
     name = "{{ cookiecutter.app_name }}"
+    # Distinct label so a service named after a Django builtin (e.g. "auth")
+    # doesn't collide with django.contrib.* app labels.
+    label = "{{ cookiecutter.app_name }}_app"
